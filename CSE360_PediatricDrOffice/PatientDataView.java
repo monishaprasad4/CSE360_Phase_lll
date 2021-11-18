a96a2760add45002df7f662d82b9ecb0159249b3
monishaprasad4 <monisha.r.prasad@gmail.com>
2021-11-17 22:49:33 -0700

    Last Visit and Next Visit dates are derived from the appointment list.  They were hardcoded.

--- CSE360_PediatricDrOffice/PatientDataView.java
+++ CSE360_PediatricDrOffice/PatientDataView.java
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class PatientDataView 
 {
@@ -11,8 +15,24 @@ public class PatientDataView
 	{
 		fullName = patient.getLastName() + ", " + patient.getFirstName();
 		doctor = currentITService.getUserFullNameFromUniqueID(patient.getDoctorUniqueID());
-		lastVisit = "1/1/1970";
-		nextVisit = "1/1/1980";
+		lastVisit = "";
+		nextVisit = "11/18/2021";
+		
+		Date today = new Date();
+		today.setHours(0);
+		
+		ArrayList<Appointment> apps = currentITService.getAppointmentsForUser(patient.getUniqueID());
+		for(int i = 0; i < apps.size(); i++)
+		{
+			if (apps.get(i).getApptDate().after(today))
+			{
+				nextVisit = apps.get(i).getApptDate().getMonth()+1 + "/" + apps.get(i).getApptDate().getDate() + "/" + (apps.get(i).getApptDate().getYear()+1900); 
+			}
+			if (apps.get(i).getApptDate().before(today))
+			{
+				lastVisit = apps.get(i).getApptDate().getMonth()+1 + "/" + apps.get(i).getApptDate().getDate() + "/" + (apps.get(i).getApptDate().getYear()+1900); 
+			} 
+		}
 		this.patient = patient;
 	}
 	
